d064739cc49183da3fa17607e050dfe23729507a
cer7 <cer7@kth.se>
2022-04-03 17:03:29 +0200

    Camera movement implemented via WASD, zoom via scrollwheel and limiters, currently rotation is yet to be added. If needed in the future, depending on UI I can implement a system for moving camera based on mouse position related to window edge
    If camera dosent work, please make sure the script it attached to the camera object and all the public variables are present
    
    Early model of the treasure ship as been added as galleon.fbx, simple mono color textures may be the most useful right now.
    Currently there is an error about self-interfecting geometry, dosent seem to affect the usage of the ship but I will fix that soon

---
+++ BonVoyage/Assets/Script/CameraMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    //controls camera speed
+    public float movementSpeed = 20f;
+
+    //limiters to how far the camera can go in each direction
+    //these values can be accessed via the inspector in unity
+    public float limiter_x = 100f; 
+    public float limiter_z = 100f;
+
+    //variable to handle scroll speed for zoom
+    public float scrollSpeed = 2000f;
+    public float minLimiter_y = 20f;
+    public float maxLimiter_y = 120f;
+    // Update is called once per frame
+    void Update()
+    {   
+        //stores current coordinate as a variable
+        Vector3 CamPos = transform.position;
+        //takes inputs for all directions at once and then applies it all at the same time at the end
+        if(Input.GetKey("w"))
+        {
+            CamPos.z += movementSpeed * Time.deltaTime;
+        }
+        
+        if(Input.GetKey("s"))
+        {
+            CamPos.z -= movementSpeed * Time.deltaTime;
+        }
+        
+        if(Input.GetKey("d"))
+        {
+            CamPos.x += movementSpeed * Time.deltaTime;
+        }
+        
+        if(Input.GetKey("a"))
+        {
+            CamPos.x -= movementSpeed * Time.deltaTime;
+        }
+
+        //Handles zoom via mousescrolling by checking speed and direction of scroll, uses unitys built in input manager for the scroll variable
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        CamPos.y += scroll * scrollSpeed * Time.deltaTime;
+        //zoom limiter
+        CamPos.y = Mathf.Clamp(CamPos.y, minLimiter_y, maxLimiter_y);
+        //makes sure that the values cant go beyond the limiters
+        CamPos.x = Mathf.Clamp(CamPos.x, -limiter_x, limiter_x);
+        CamPos.z = Mathf.Clamp(CamPos.z, -limiter_z, limiter_z);
+
+        //applies all changes
+        transform.position = CamPos;
+    }
+}
