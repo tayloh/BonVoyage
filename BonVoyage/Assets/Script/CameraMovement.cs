f2ea84e15607d927c4b3e91433e6825b399eb2a2
cer7 <cer7@kth.se>
2022-04-03 17:40:03 +0200

    Rotation added, currently limited to x axis, to unlock y axis rotation or shift to y rotation check the out commented code

--- BonVoyage/Assets/Script/CameraMovement.cs
+++ BonVoyage/Assets/Script/CameraMovement.cs
@@ -16,6 +16,11 @@ public class CameraMovement : MonoBehaviour
     public float scrollSpeed = 2000f;
     public float minLimiter_y = 20f;
     public float maxLimiter_y = 120f;
+
+    //Speed of rotation
+    public float RotSpeed = 6f;
+
+
     // Update is called once per frame
     void Update()
     {   
@@ -53,5 +58,12 @@ public class CameraMovement : MonoBehaviour
 
         //applies all changes
         transform.position = CamPos;
+
+        if(Input.GetMouseButton(1))
+        {
+            transform.eulerAngles += RotSpeed * new Vector3(-Input.GetAxis("Mouse Y"), 0, 0);
+            //transform.eulerAngles += RotSpeed * new Vector3(0, Input.GetAxis("Mouse X"), 0);
+            //transform.eulerAngles += RotSpeed * new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+        }
     }
 }
