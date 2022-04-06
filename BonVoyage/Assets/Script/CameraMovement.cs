132b93ce64ec6999d90cd7446f574c112e45c1fc
Erik Stolpe <erik.stolpe98@gmail.com>
2022-04-06 18:45:26 +0200

    Fix camera movement direction and clamp rotation

--- BonVoyage/Assets/Script/CameraMovement.cs
+++ BonVoyage/Assets/Script/CameraMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,38 +19,66 @@ public class CameraMovement : MonoBehaviour
     public float maxLimiter_y = 120f;
 
     //Speed of rotation
-    public float RotSpeed = 6f;
+    public float RotSpeed = 2000f;
+
+    public int RotMaxX = 90;
+    public int RotMinX = 0;
 
 
-    // Update is called once per frame
-    void Update()
+    // option = 0 => camera z axis
+    // option = 1 => camera x axis
+    private Vector3 _getCameraMoveDirection(int option)
+    {
+        switch (option)
+        {
+            case 0: 
+                return new Vector3(transform.forward.x, 0, transform.forward.z).normalized;
+            case 1:
+                return new Vector3(transform.right.x, 0, transform.right.z).normalized;
+            default:
+                throw new Exception($"Invalid option {option}");
+            
+        }
+    }
+
+    private void LateUpdate()
     {
         //stores current coordinate as a variable
         Vector3 CamPos = transform.position;
         //takes inputs for all directions at once and then applies it all at the same time at the end
+
+        Vector3 resultingMoveDir = Vector3.zero;
+
         if (Input.GetKey("w"))
         {
-            CamPos.z += movementSpeed * Time.deltaTime;
+            resultingMoveDir += _getCameraMoveDirection(0);
+            //CamPos += _getCameraMoveDirection(0) * movementSpeed * Time.deltaTime;
         }
-
-        if (Input.GetKey("s"))
+        else if (Input.GetKey("s"))
         {
-            CamPos.z -= movementSpeed * Time.deltaTime;
+            resultingMoveDir -= _getCameraMoveDirection(0);
+            //CamPos -= _getCameraMoveDirection(0) * movementSpeed * Time.deltaTime;
         }
 
         if (Input.GetKey("d"))
         {
-            CamPos.x += movementSpeed * Time.deltaTime;
+            resultingMoveDir += _getCameraMoveDirection(1);
+            //CamPos += _getCameraMoveDirection(1) * movementSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey("a"))
+        {
+            resultingMoveDir -= _getCameraMoveDirection(1);
+            //CamPos -= _getCameraMoveDirection(1) * movementSpeed * Time.deltaTime;
         }
 
-        if (Input.GetKey("a"))
-        {
-            CamPos.x -= movementSpeed * Time.deltaTime;
-        }
+        // Calculate new position
+        resultingMoveDir = resultingMoveDir.normalized;
+        CamPos += resultingMoveDir * movementSpeed * Time.deltaTime;
 
         //Handles zoom via mousescrolling by checking speed and direction of scroll, uses unitys built in input manager for the scroll variable
         float scroll = Input.GetAxis("Mouse ScrollWheel");
-        CamPos.y += scroll * scrollSpeed * Time.deltaTime;
+
+        CamPos += transform.forward * scroll * scrollSpeed * Time.deltaTime;
         //zoom limiter
         CamPos.y = Mathf.Clamp(CamPos.y, minLimiter_y, maxLimiter_y);
         //makes sure that the values cant go beyond the limiters
@@ -61,8 +90,16 @@ public class CameraMovement : MonoBehaviour
 
         if (Input.GetMouseButton(1))
         {
-            transform.eulerAngles += RotSpeed * new Vector3(-Input.GetAxis("Mouse Y"), 0, 0);
+            //transform.eulerAngles += RotSpeed * new Vector3(-Input.GetAxis("Mouse Y"), 0, 0);
             //transform.eulerAngles += RotSpeed * new Vector3(0, Input.GetAxis("Mouse X"), 0);
+            Vector3 currentEulerAngles = transform.eulerAngles;
+
+            Vector3 eulerAnglesRotation = Time.deltaTime * RotSpeed * new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+            Vector3 resultingEulerAngles = currentEulerAngles + eulerAnglesRotation;
+
+            resultingEulerAngles.x = Mathf.Clamp(resultingEulerAngles.x, RotMinX, RotMaxX);
+
+            transform.eulerAngles = resultingEulerAngles;
             //transform.eulerAngles += RotSpeed * new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
         }
     }
