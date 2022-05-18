3f94bb02b3f36fc551f6fc7ed44c281c3c5703f7
Erik Stolpe <erik.stolpe98@gmail.com>
2022-05-18 14:54:22 +0200

    Add async load with loading bar

--- BonVoyage/Assets/Script/MainMenu.cs
+++ BonVoyage/Assets/Script/MainMenu.cs
@@ -2,23 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    
+
+    [SerializeField]
+    private Slider _LoadingBar;
+
+    public void LoadLevel (int sceneIndex)
+    {
+        StartCoroutine(LoadLevelAsync(sceneIndex));
+    }
+
+    private IEnumerator LoadLevelAsync(int sceneIndex)
+    {
+        _LoadingBar.gameObject.SetActive(true);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        var loadText = _LoadingBar.GetComponentInChildren<TMPro.TextMeshProUGUI>();
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            _LoadingBar.value = progress;
+            loadText.text = progress * 100 + "%";
+
+            yield return null;
+        }
+    } 
+
     public void PlayLevel1()
     {
-        SceneManager.LoadScene(1);
+        LoadLevel(1);
     }
 
     public void PlayLevel2()
     {
-        SceneManager.LoadScene(2);
+        LoadLevel(2);
     }
 
     public void PlayLevel10()
     {
-        SceneManager.LoadScene(3);
+        LoadLevel(3);
     }
 
     public void QuitGame()
